b6880b175da9c05a56f7fbb193f587a749e07f37
aadijotwani <aadijotwani@gmail.com>
2025-05-28 14:01:46 +0530

    intro to javascript

---
+++ JavaScript/task1/script.js
@@ -0,0 +1,7 @@
+function submit() {
+  const nm = document.getElementById("fullName");
+  console.log("Name", nm.value);
+
+  const ps = document.getElementById("password");
+  console.log("password", ps.value);
+}
