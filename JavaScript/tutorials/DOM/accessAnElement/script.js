dc15ba7680ad67cb1f6994f13c5617c705ef39ec
aadijotwani <aadijotwani@gmail.com>
2025-05-29 14:41:19 +0530

    changing content using js

--- JavaScript/tutorials/DOM/accessAnElement/script.js
+++ JavaScript/tutorials/DOM/accessAnElement/script.js
@@ -1,10 +1,7 @@
 // Get the <h1> and <p> element by its ID
-let heading = document.getElementById("header")
-let paragraph = document.getElementById("info")
-console.log(heading.innerText) //Output: Welcome to my Page
-console.log(paragraph.innerText) //Output: This is a paragraph 
-
-
-
+let heading = document.getElementById("header");
+let paragraph = document.getElementById("info");
+console.log(heading.innerText); //Output: Welcome to my Page
+console.log(paragraph.innerText); //Output: This is a paragraph
 
 
