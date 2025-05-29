8ece3ceef0a0342a6c0aefea97e7fdca1e8b5cc9
aadijotwani <aadijotwani@gmail.com>
2025-05-29 14:26:44 +0530

    accessing an element using js

---
+++ JavaScript/tutorials/DOM/accessAnElement/script.js
@@ -0,0 +1,10 @@
+// Get the <h1> and <p> element by its ID
+let heading = document.getElementById("header")
+let paragraph = document.getElementById("info")
+console.log(heading.innerText) //Output: Welcome to my Page
+console.log(paragraph.innerText) //Output: This is a paragraph 
+
+
+
+
+
