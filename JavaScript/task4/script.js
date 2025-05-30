22d752980f4ae39e63c2ce9690b60ec3eba44c7a
aadijotwani <aadijotwani@gmail.com>
2025-05-30 16:33:57 +0530

    external color button for changing color using js

---
+++ JavaScript/task4/script.js
@@ -0,0 +1,17 @@
+function changeColorBlock(color) {
+  const colorBlock = document.getElementById("outerBlock");
+  colorBlock.style.backgroundColor = color;
+}
+
+
+function changeColorHeading(color)
+{
+    const colorBlock = document.getElementById("heading");
+    colorBlock.style.color = color;
+}
+
+function changeColorParagraph(color)
+{
+  const colorBlock = document.getElementById("paragraph")
+  colorBlock.style.color = color;
+}
