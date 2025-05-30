0f3113dbe7a8203e3dbef7da8e2704fac62d6c2c
aadijotwani <aadijotwani@gmail.com>
2025-05-30 19:57:53 +0530

    changes

---
+++ JavaScript/tutorials/usingTextContent/script.js
@@ -0,0 +1,13 @@
+  let div = document.getElementById("myDiv");
+      console.log(div.textContent);
+
+      //In console
+      /* Output: "Hello World!"
+                  I am    Aadi
+                  
+                  
+                  I am
+
+                  Aadi
+                  
+                  */
