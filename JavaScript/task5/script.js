bddb2fd9b89a6d028d168c60223208bb63e0d879
aadijotwani <aadijotwani@gmail.com>
2025-05-31 15:52:43 +0530

    extra

--- JavaScript/task5/script.js
+++ JavaScript/task5/script.js
@@ -1,6 +1,34 @@
 function bulbColor(color) {
-    
-    const inputColor = document.getElementById("circle");
+  const inputColor = document.getElementById("circle");
 
-    inputColor.style.backgroundColor = color;
+  inputColor.style.backgroundColor = color;
 }
+
+const rb = document.getElementById("red");
+const gb = document.getElementById("green");
+const bb = document.getElementById("blue");
+
+rb.addEventListener("mouseover", () => {
+  bulbColor("red");
+});
+
+gb.addEventListener("mouseover", () => {
+  bulbColor("green");
+});
+
+bb.addEventListener("mouseover", () => {
+  bulbColor("blue");
+});
+
+rb.addEventListener("mouseout", () => {
+  bulbColor("white");
+});
+
+gb.addEventListener("mouseout", () => {
+  bulbColor("white");
+});
+
+bb.addEventListener("mouseout", () => {
+  bulbColor("white");
+});
+
