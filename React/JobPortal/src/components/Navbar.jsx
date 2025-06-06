f0db8b4f4613178f146572a9968a9d73b6a09a51
aadijotwani <aadijotwani@gmail.com>
2025-06-06 20:57:11 +0530

    login created

--- React/JobPortal/src/components/Navbar.jsx
+++ React/JobPortal/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="p-5 bg-white text-black flex justify-center gap-45 items-center sticky top-0 z-99">
+      <div className="p-5 bg-white text-black flex justify-center gap-70 items-center sticky top-0 z-99 w-screen">
         <img src={logo} alt="logo" className="h-15" />
         <div className="flex gap-11 font-sans">
           <NavLink to={"/"} className="text-black hover:text-[#F54677] ">
@@ -25,10 +25,10 @@ const Navbar = () => {
           </NavLink>
         </div>
         <div className="flex gap-3">
-          <button className="py-4 border px-13 hover:bg-[#F54677] hover:text-white" onClick={()=>navigate("/register")}>
+          <button className="py-4 border px-13 bg-[#F54677] text-white hover:bg-white hover:text-black rounded-[20px]" onClick={()=>navigate("/register")}>
             Register
           </button>
-          <button className="py-4 px-8 border hover:bg-[#F54677] hover:text-white"onClick={()=>navigate("/login")}>
+          <button className="py-4 px-8 border hover:bg-[#F54677] hover:text-white rounded-[20px]"onClick={()=>navigate("/login")}>
             Login
           </button>
         </div>
--- React/JobPortal/src/pages/Login.jsx
+++ React/JobPortal/src/pages/Login.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className='bg-gradient-to-r from-pink-200 to-blue-200 w-screen h-[88.5vh] flex items-center justify-center' >
+        <div className='h-[55%] w-[27%] bg-white/30 rounded-[40px] hover:shadow-gray-500 hover:shadow-xl hover:transition duration-1800 '>
+          <h1 className='mt-2 ml-6 text-[45px] font-bold bg-gradient-to-r from-gray-900 to-red-700 bg-clip-text text-transparent'>Login</h1>
+
+          <div className='ml-7'>
+            <label htmlFor="UserID">User ID:</label>
+            <input type="text" name="Userid" id="UserID" className='border-b w-[75%]'/>
+          </div>
+        </div>
+    </div>
+    
+    </>
   )
 }
 
