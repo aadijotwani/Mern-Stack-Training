9486605d879da53da3c5c1714c23c05d35a85334
aadijotwani <aadijotwani@gmail.com>
2025-06-14 18:57:15 +0530

    link used in npm run dev

--- Node/firstBackend/index.js
+++ Node/firstBackend/index.js
@@ -2,14 +2,14 @@ import express from "express";
 
 const app = express(); // ham express ka object bana rahe hai jisko naam app de rahe hai.
 
+//app.get(path, callback Function)
 
 app.get("/", (req, res) => {
-    console.log("Server Connceted")
-     res.json({ message: "Hello, I am Backend!"});
-})
+  console.log("Server Connceted");
+ res.json({ message: "Hello, I am Backend!" });
+});
 
+app.listen(3012, () => {
+  console.log("http://localhost:3012");
 
-app.listen(3000, () => {
-    console.log("Server Started")
-
-}) 
+});
